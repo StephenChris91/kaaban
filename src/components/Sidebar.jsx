8dfb9e9a2716239f1bbaf9c441ef9b62f931e5b7
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-02-22 02:51:37 +0100

    added authentication

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     return ( 
         <>
             <div className="h-screen w-64 p-5 shadow-sm bg-slate-900">
-                <h1 className="text-white mb-6">Sidebar</h1>
+                <h1 className="text-white mb-6 font-medium text-4xl">KAANBAN</h1>
                 <Menu />
             </div>
         </>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,9 +4,15 @@ import Modal from '../components/modal';
 import Sidebar from "../components/Sidebar";
 import { auth } from '../firebase/config'
 
+//context
+import { useContext } from "react";
+import { AuthContext } from "../firebase/auth/AuthContext";
+
 const Dasboard = () => {
 
     const [showModal, setShowModal ] = useState(false)
+
+    const user = useContext(AuthContext)
 
     const handleOpenModal = () => {
         setShowModal(true);
@@ -22,10 +28,24 @@ const Dasboard = () => {
     return ( 
         <>
         <Layout>
-            <Sidebar />
-            <Modal showModal={showModal} onClose={handleCloseModal}/>
-            <button className='fixed top-5 right-5 p-3 rounded-sm w-40 bg-slate-900 text-white' onClick={handleOpenModal}> Create</button>
-            <button className='fixed top-5 left-72 p-3 rounded-sm w-40 bg-slate-900 text-white' onClick={handleSignOut}>Log Out</button>
+          <div className='flex w-screen'>
+            <div>
+              <Sidebar />
+            </div>
+            <div className='w-screen p-2'>
+              <Modal showModal={showModal} onClose={handleCloseModal}/>
+              <div className='flex justify-between w-full items mb-10'>
+                <button className='p-3 rounded-sm w-40 bg-slate-900 text-white' onClick={handleOpenModal}>Create</button>
+                <button className='p-3 rounded-sm w-40 bg-slate-900 text-white' onClick={handleSignOut}>Log Out</button>
+              </div>
+              <div className='flex flex-col justify-content'>
+                <div className='flex flex-col'>
+                  <span>Welcome</span><h1 className='font-normal text-4xl'>{user.displayName}</h1>
+                </div>
+              </div>
+              </div>
+          </div>
+            
         </Layout>
         </>
      );
